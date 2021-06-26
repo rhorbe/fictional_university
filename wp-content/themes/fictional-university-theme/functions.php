cbf026ff2e37ac118ecfc324f15c0aba158e5b63
Rafael Orbe <rafael@x360.local>
2021-06-26 19:17:14 -0300

    Likes finalizado

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+require get_theme_file_path('/inc/like-route.php');
 require get_theme_file_path('/inc/search-route.php');
 
 function university_custom_rest()
@@ -61,8 +62,8 @@ function university_files()
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.c3ece7e966dcb0202176.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.8e869ad4493f86dc920b.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.8e869ad4493f86dc920b.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.305cfe026af0847fdc52.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.305cfe026af0847fdc52.css'));
   }
 
   wp_localize_script('main-university-js', 'universityData', array(
@@ -160,7 +161,7 @@ add_action('login_enqueue_scripts', 'ourLoginCSS');
 function ourLoginCSS()
 {
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.8e869ad4493f86dc920b.css'));
+  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.305cfe026af0847fdc52.css'));
 }
 
 add_filter('login_headertitle', 'ourLoginTitle');
--- wp-content/themes/fictional-university-theme/js/modules/Like.js
+++ wp-content/themes/fictional-university-theme/js/modules/Like.js
@@ -11,19 +11,55 @@ class Like {
 
   ourClickDispatcher(e) {
     const currentLikeBox = $(e.target).closest(".like-box");
-    if (currentLikeBox.data("exists") == "yes") {
-      this.deleteLike();
+    if (currentLikeBox.attr("data-exists") == "yes") {
+      this.deleteLike(currentLikeBox);
     } else {
-      this.createLike();
+      this.createLike(currentLikeBox);
     }
   }
 
-  createLike() {
-    alert("create");
+  createLike(currentLikeBox) {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+      },
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      type: "POST",
+      data: { professorId: currentLikeBox.data("professor") },
+      success: (response) => {
+        currentLikeBox.attr("data-exists", "yes");
+        let likeCount = parseInt(currentLikeBox.find(".like-count").html());
+        likeCount++;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", response);
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
   }
 
-  deleteLike() {
-    alert("delete");
+  deleteLike(currentLikeBox) {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+      },
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      data: { like: currentLikeBox.attr("data-like") },
+      type: "DELETE",
+      success: (response) => {
+        currentLikeBox.attr("data-exists", "no");
+        let likeCount = parseInt(currentLikeBox.find(".like-count").html());
+        likeCount--;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", '');
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
   }
 }
 
